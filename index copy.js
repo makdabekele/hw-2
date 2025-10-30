9425a6a935afdadad7b2a5364a12a7c5f60ec462
makdabekele <makda.bekele@gwu.edu>
2025-10-30 13:09:53 -0400

    Add initial HTML structure for the website

---
+++ index copy.js
@@ -0,0 +1,20 @@
+<html>
+<!-- 
+//css -->
+<style>
+    h1 { 
+        color: red
+    }
+</style>
+
+<head>
+    <title> My Website</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
+</head>
+
+    <body>
+        <h1> Welcome to My Website</h1>
+        <p></p>
+        <script src = "simple.js"></script>
+    </body>
+</html>
